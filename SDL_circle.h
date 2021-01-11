84b4efa61f8ee6f72897114e023f862f64c3887c
Comrade Riley <50218871+Comrade-Riley@users.noreply.github.com>
2021-01-11 09:45:09 -0500

    Add files via upload

--- SDL_circle.h
+++ SDL_circle.h
@@ -1,52 +1,28 @@
-#include <chrono>
-#include <thread>
-#include <future>
 #include <SDL2/SDL.h>
-#include <stdio.h>
-#include <string>
-#include <vector>
 
 //source https://gist.github.com/derofim/912cfc9161269336f722
 /*
-Edits: Removed extra things to turn it into a c++ file 
+Edits: Removed extra things to turn it into a .h file 
 edited the includes
 added circle class to make it more like SDL_RendererDrawRect
 added circle class to make it look better
-renamed some things
+renamed some things to clean the code up and make it more like SDL_RendererDrawRect/SDL_RendererFillRect
 */ 
 
-
-
-
-
-struct SDL_Circle{
+typedef struct SDL_Circle{
 	int x;
 	int y;
 	int radius;
-};
+} SDL_Circle;
 
 struct SDL_color{int r;int g;int b;int a;};
 
-void SDL_RenderSetPixel(SDL_Renderer *rend, int x, int y, SDL_Color c)
-{
-	Uint8 r = c.r;
-	Uint8 g = c.g;
-	Uint8 b = c.b;
-	Uint8 a = c.a;
-	SDL_SetRenderDrawColor(rend, r,g,b,a);
-	SDL_RenderDrawPoint(rend, x, y);
-}
-
-//ex. SDL_RenderDrawCircle(renderer,circle,{0,0,0,255})
-void SDL_RenderDrawCircle(SDL_Renderer *surface, SDL_Circle cir, SDL_Color c)
+//ex. SDL_RenderDrawCircle(renderer,circle)
+void SDL_RenderDrawCircle(SDL_Renderer *surface, SDL_Circle cir)
 {
 	int n_cx = cir.x;
 	int n_cy = cir.y;
 	int radius = cir.radius;
-	Uint8 r = c.r;
-	Uint8 g = c.g;
-	Uint8 b = c.b;
-	Uint8 a = c.a;
 	
 	// if the first pixel in the screen is represented by (0,0) (which is in sdl)
 	// remember that the beginning of the circle is not in the middle of the pixel
@@ -60,25 +36,25 @@ void SDL_RenderDrawCircle(SDL_Renderer *surface, SDL_Circle cir, SDL_Color c)
 
 	while (x >= y)
 	{
-		SDL_RenderSetPixel(surface, (int)(cx + x), (int)(cy + y), {r, g, b, a});
-		SDL_RenderSetPixel(surface, (int)(cx + y), (int)(cy + x), {r, g, b, a});
+		SDL_RenderDrawPoint(surface, (int)(cx + x), (int)(cy + y));
+		SDL_RenderDrawPoint(surface, (int)(cx + y), (int)(cy + x));
 
 		if (x != 0)
 		{
-			SDL_RenderSetPixel(surface, (int)(cx - x), (int)(cy + y), {r, g, b, a});
-			SDL_RenderSetPixel(surface, (int)(cx + y), (int)(cy - x), {r, g, b, a});
+			SDL_RenderDrawPoint(surface, (int)(cx - x), (int)(cy + y));
+			SDL_RenderDrawPoint(surface, (int)(cx + y), (int)(cy - x));
 		}
 
 		if (y != 0)
 		{
-			SDL_RenderSetPixel(surface, (int)(cx + x), (int)(cy - y), {r, g, b, a});
-			SDL_RenderSetPixel(surface, (int)(cx - y), (int)(cy + x), {r, g, b, a});
+			SDL_RenderDrawPoint(surface, (int)(cx + x), (int)(cy - y));
+			SDL_RenderDrawPoint(surface, (int)(cx - y), (int)(cy + x));
 		}
 
 		if (x != 0 && y != 0)
 		{
-			SDL_RenderSetPixel(surface, (int)(cx - x), (int)(cy - y), {r, g, b, a});
-			SDL_RenderSetPixel(surface, (int)(cx - y), (int)(cy - x), {r, g, b, a});
+			SDL_RenderDrawPoint(surface, (int)(cx - x), (int)(cy - y));
+			SDL_RenderDrawPoint(surface, (int)(cx - y), (int)(cy - x));
 		}
 
 		error += y;
@@ -91,25 +67,15 @@ void SDL_RenderDrawCircle(SDL_Renderer *surface, SDL_Circle cir, SDL_Color c)
 			error -= x;
 			error -= x;
 		}
-		/*
-		// sleep for debug
-		SDL_RenderPresent(gRenderer);
-		std::this_thread::sleep_for(std::chrono::milliseconds{ 100 });
-		*/
 	}
 }
 
-//ex. SDL_RenderFillCircle(renderer,circle,{0,0,0,255})
-void SDL_RenderFillCircle(SDL_Renderer* gRenderer, SDL_Circle cir, SDL_Color c)
+//ex. SDL_RenderFillCircle(renderer,circle)
+void SDL_RenderFillCircle(SDL_Renderer* gRenderer, SDL_Circle cir)
 {
 	int cx = cir.x;
 	int cy = cir.y;
 	int radius = cir.radius;	
-	Uint8 r = c.r;
-	Uint8 g = c.g;
-	Uint8 b = c.b;
-	Uint8 a = c.a;
-	
 	// Note that there is more to altering the bitrate of this 
 	// method than just changing this value.  See how pixels are
 	// altered at the following web page for tips:
@@ -124,13 +90,11 @@ void SDL_RenderFillCircle(SDL_Renderer* gRenderer, SDL_Circle cir, SDL_Color c)
 		// height of the circle.  The result is used to draw a scan line and
 		// its mirror image below it.
 
-		// The following formula has been simplified from our original.  We
-		// are using half of the width of the circle because we are provided
+		// We are using half of the width of the circle because we are provided
 		// with a center and we need left/right coordinates.
 
 		double dx = floor(sqrt((2.0 * radius * dy) - (dy * dy)));
 		int x = cx - dx;
-		SDL_SetRenderDrawColor(gRenderer, r, g, b, a);
 		SDL_RenderDrawLine(gRenderer, cx - dx, cy + dy - radius, cx + dx, cy + dy - radius);
 		SDL_RenderDrawLine(gRenderer, cx - dx, cy - dy + radius, cx + dx, cy - dy + radius);
 
@@ -144,10 +108,5 @@ void SDL_RenderFillCircle(SDL_Renderer* gRenderer, SDL_Circle cir, SDL_Color c)
 			target_pixel_a += BPP;
 			target_pixel_b += BPP;
 		}*/
-		/*
-		// sleep for debug
-		SDL_RenderPresent(gRenderer);
-		std::this_thread::sleep_for(std::chrono::milliseconds{ 100 });
-		*/
 	}
 }
