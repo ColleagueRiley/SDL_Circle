486dbd82c9bde3f29986de36e36e75ec949c2dc3
Comrade Riley <50218871+Comrade-Riley@users.noreply.github.com>
2021-01-30 23:30:18 -0500

    Delete make.bat

--- make.bat
+++
@@ -1,8 +0,0 @@
-@echo "setting vars"
-SET LIBRARIES=-lSDL2 -lSDL2_mixer
-SET NAME=example.exe
-SET BUILD=build
-SET FILES=example.cpp
-
-@echo "Creating file"
-g++ %FILES% %LIBRARIES% -o %BUILD%\%NAME%
